d48c9694bdb03c032d3e2dcf5df2c4c102634724
Matheus Camacho <6325656+Matheus-camacho@users.noreply.github.com>
2018-10-24 19:53:15 +0000

    Add files via upload

--- finalArduino/finalArduino.ino
+++ finalArduino/finalArduino.ino
@@ -6,15 +6,14 @@ Servo myServo;
 #define MOTOR_PIN 9
 
 int DELAY = 1000; 
-int servoPin = 9; 
-int motorPin=10;  
+int servoPin = 10; 
+int motorPin=9;  
 int servoAngle = 0; 
 
 
 void setup() {      
   myServo.attach(servoPin);
   Serial.begin(9600);
-  Serial.println("Don't forget to subscribe!");
   Serial.println("ELECTRONOOBS ESC calibration...");
   Serial.println(" ");
   delay(1500);
@@ -43,6 +42,8 @@ void setup() {
   Serial.println("and the motor will start rotating.");
   Serial.println("Send 1000 to stop the motor and 2000 for full throttle");
 } 
+
+
 int pos = 0;
 void loop()
 { 
@@ -76,10 +77,5 @@ void loop()
       myServo.detach();  
       
       } 
-  }
-
-  if (Serial.available()>0){
-    int command=Serial.read();
-    
   }
 }
